7fbf83c60eccff0bd7a4cd63f3dcf17ffb86f619
shobh <shobh@LAPTOP-2B3VV4RM.attlocal.net>
2022-08-31 00:15:04 -0400

    Added Generic sleep Method

--- src/main/java/acti/utils/Helper.java
+++ src/main/java/acti/utils/Helper.java
@@ -2,4 +2,14 @@ package acti.utils;
 
 public class Helper {
 
+	
+	public void fn_sleep( ) {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
 }
